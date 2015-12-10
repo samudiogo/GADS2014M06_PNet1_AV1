7a364a007637490ed9a775b729cd2098d6f3ed05
Supremo Samuel Diogo <samudiogo@gmail.com>
2015-12-10 11:07:35 -0200

    criação da entidade sessão

--- GADS2014M06_PNet1_AV1.Domain/Entidades/Serie.cs
+++ GADS2014M06_PNet1_AV1.Domain/Entidades/Serie.cs
@@ -2,9 +2,9 @@
 
 namespace GADS2014M06_PNet1_AV1.Domain.Entidades
 {
+
     public class Serie
     {
-        // Nome, criador, País (PaisId), Gênero (GeneroId), sinopse, qual o Canal de TV (CanalDeTvId) oficial que apresenta a série, qual o dia da semana e horário do episódio e uma foto de capa (caminho da foto).
         public Guid Id { get; set; }
         public string Nome { get; set; }
         public string Sinopse { get; set; }
@@ -12,9 +12,13 @@ namespace GADS2014M06_PNet1_AV1.Domain.Entidades
         public DateTime DataHoraApresentacao { get; set; }
         public string ImagemCapa { get; set; }
 
+
+        //representando os relacionamentos exeternos:
         public int GeneroId { get; set; }
         public int PaisId { get; set; }
         public int CanalTvId { get; set; }
+
+        
 
     }
 }
---
+++ GADS2014M06_PNet1_AV1.Domain/Entidades/Sessao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GADS2014M06_PNet1_AV1.Domain.Entidades
+{
+    /// <summary>
+    /// Sessão representa a temporada da serie
+    /// </summary>
+    public class Sessao
+    {
+        /// <summary>
+        /// Id : S01, S02
+        /// </summary>
+        public string Id { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Sinopse { get; set; }
+        public string ImagemCapa { get; set; }
+
+    }
+}
