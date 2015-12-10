1d3fa3e1108d9476ad73c96502744fa2569a76e8
Supremo Samuel Diogo <samudiogo@gmail.com>
2015-12-10 10:48:09 -0200

    criação das entidades serie e gênero

---
+++ GADS2014M06_PNet1_AV1.Domain/Entidades/Genero.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GADS2014M06_PNet1_AV1.Domain.Entidades
+{
+    public class Genero
+    {
+        public Guid Id { get; set; }
+        public string Descricao { get; set; } 
+    }
+}
---
+++ GADS2014M06_PNet1_AV1.Domain/Entidades/Serie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GADS2014M06_PNet1_AV1.Domain.Entidades
+{
+    public class Serie
+    {
+        // Nome, criador, País (PaisId), Gênero (GeneroId), sinopse, qual o Canal de TV (CanalDeTvId) oficial que apresenta a série, qual o dia da semana e horário do episódio e uma foto de capa (caminho da foto).
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Sinopse { get; set; }
+
+
+    }
+}
