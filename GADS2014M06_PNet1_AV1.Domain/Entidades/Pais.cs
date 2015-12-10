79fd16289c8bc3196dd83508c8ab535e4e5098e5
Supremo Samuel Diogo <samudiogo@gmail.com>
2015-12-10 10:56:52 -0200

    criando as classes canaltv pais e adicionando mais propredades na classe serie

---
+++ GADS2014M06_PNet1_AV1.Domain/Entidades/Pais.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GADS2014M06_PNet1_AV1.Domain.Entidades
+{
+    public class Pais
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
--- GADS2014M06_PNet1_AV1.Domain/Entidades/Serie.cs
+++ GADS2014M06_PNet1_AV1.Domain/Entidades/Serie.cs
@@ -9,6 +9,12 @@ namespace GADS2014M06_PNet1_AV1.Domain.Entidades
         public string Nome { get; set; }
         public string Sinopse { get; set; }
 
+        public DateTime DataHoraApresentacao { get; set; }
+        public string ImagemCapa { get; set; }
+
+        public int GeneroId { get; set; }
+        public int PaisId { get; set; }
+        public int CanalTvId { get; set; }
 
     }
 }
